667d371e89bdd9b567bff1b2c183f6fed199daa6
tanggangli <991463839@qq.com>
2019-12-06 17:56:29 +0800

    Initial commit

---
+++ src/main/java/com/meterbox/attendance/backstage/AttendanceBackstageApplication.java
@@ -0,0 +1,13 @@
+package com.meterbox.attendance.backstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceBackstageApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/meterbox/attendance/backstage/dao/Test.java
@@ -0,0 +1,9 @@
+package com.meterbox.attendance.backstage.dao;
+
+/**
+ * @Description: 文件描述
+ * @Author: CEM
+ * @Date: 2019/12/4 15:53
+ **/
+public class Test {
+}
